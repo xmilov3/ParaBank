ff69c3e02f66a7681d4f7ac85388fe0333302418
xmilov3 <xmilov3@gmail.com>
2024-09-16 17:57:06 +0200

    Added test results reporting tool, changed tomcat to be in repository

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,21 @@
 const { defineConfig } = require("cypress");
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
 
-//module.exports = defineConfig({
-//e2e: {
-//setupNodeEvents(on, config) {
-// implement node event listeners here
-//},
-//},
-//});
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      mochawesome(on);
+      return config;
+    },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      charts: true,
+      reportPageTitle: "Cypress Test Results",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+    },
     baseUrl: "http://localhost:8080/parabank/index.htm",
     supportFile: "cypress/support/e2e.js",
     specPattern: "cypress/e2e/**/*.spec.js",
