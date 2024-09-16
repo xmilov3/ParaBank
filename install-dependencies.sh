cd569c835833a79f26e7884060f65459614d02d3
xmilov3 <xmilov3@gmail.com>
2024-09-16 19:03:33 +0200

    added script to install dependencies, change runs-on to ubuntu-latest

---
+++ install-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# This script will download all dependencies from package.json
+# You don't have to set the path to your package.json if it is in your main folder
+
+# cd /path/to/your/main/folder
+
+# Install all dependencies
+
+npm install
+
+# To run this script you have to enter command below in your terminal
+# ./install-dependencies.sh 
