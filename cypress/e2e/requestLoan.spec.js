9a6aeb2ca565ea7ee9fdd684b3e8ad72a60c9380
xmilov3 <xmilov3@gmail.com>
2024-09-16 22:31:38 +0200

    Final fix

--- cypress/e2e/requestLoan.spec.js
+++ cypress/e2e/requestLoan.spec.js
@@ -26,7 +26,7 @@ describe("Loan Application", () => {
 
     cy.get("#amount").type(loanAmount);
     cy.get("#downPayment").type(downPayment);
-    cy.get("select#fromAccountId").should("have.value", "13677");
+    cy.get("select#fromAccountId").select("13677");
 
     cy.get('input.button[value="Apply Now"]').click();
 
